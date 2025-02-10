da9682cd622ccc12bc729d8d48be79b2f7999436
Daniil <kalinin.de@phystech.edu>
2025-02-10 08:39:01 +0300

    Reviewed code

--- Source1.cpp
+++ Source1.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <cmath>
 
+// TODO: Makefile
+// TODO: separate files
+// TODO: remove .vs folder
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    double a, b, c;
+    double a, b, c; // Initialize
+
+    // TODO make function
     std::cout << "Введите коэффициенты A, B, C: ";
     std::cin >> a >> b >> c;
-
+    /*c == inf*/
+    /*c == nan*/
+    // TODO check if is all ok with input
+    
+    // Nice
     if (a == 0) {
         std::cout << "Это не квадратное уравнение!" << std::endl;
         return 0;
     }
 
+    // TODO separate solver logic from output
+    // TODO tests
     double D = b * b - 4 * a * c; // Формула дискриминанта
 
     if (D > 0) { // Два различных корня
