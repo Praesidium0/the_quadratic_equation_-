269b54888173b2ca6bebe0585d765159f69cffa5
Daniil Kalinin <kalinin.de@star.intra.ispras.ru>
2025-02-24 15:02:51 +0300

    reviewed

--- main.cpp
+++ main.cpp
@@ -10,9 +10,31 @@ int main() {
     int input = 0;
     std::cout << "Выберите действие: \n 1. Решение квадратного уравнения \n 2. Решение линейного уравнения \n 3. Запустить тестирование \n 4. Выход \n Ввод: ";
     std::cin >> input;
-
+    
+    // TODO: What if input < 0? Or input > 4?  
+    // TODO: Never trust the user! Let him just input coefficients and then decide if equasion is linear or square.
+    //       I would suggest you to create main like that:
+    //          int main() {
+    //              input_coeffitients(a, b, c);
+    //              answer = solve_eqausion(a, b, c);
+    //              output_solution(answer);
+    //          }
+    //          
+    //      While solve_eqausion looks something like:
+    //          answer_type solve_eqausion(a, b, c) {
+    //              eq_type = determine_equasion_type(a, b, c);
+    //              switch (eq_type)
+    //                  case LINEAR:
+    //                      return solve_linear(b, c);
+    //                  case SQUARE:
+    //                      return solve_square(b, c);
+    //                  default:
+    //                      print_error("error desription");
+    //          }
+    //
+    
     if (input == 1) {
-        solution_is_square(input);
+        solution_is_square(input); // TODO: Why do you pass input here? It's unused..
 
     } else if (input == 2) {
         solution_is_linear(input);
@@ -21,7 +43,7 @@ int main() {
         testing(input);
 
     } else if (input == 4){
-        exit;
+        exit; // Exit is a function, and should be called like `exit()`, but its better to call return from main
     }
 
     return 0;
